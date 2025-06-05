1078162f7a0c3d2268e4f85a0c7bc9de574cba41
samelmayuresh <mayureshsamel5@gmail.com>
2025-06-05 18:31:45 +0530

    new

--- sc.js
+++ sc.js
@@ -12,18 +12,52 @@
 // }
 // console.log(square(5)); // Output: 25
 
-function pokemon(name, type1,type2) {
-    console.log("Pokemon Name: " + name);
-    type1(type2);
+// function pokemon(name, type1,type2) {
+//     console.log("Pokemon Name: " + name);
+//     type1(type2);
 
-}
-function pikachu(pok) {
-    console.log("Pikachu is an Electric type Pokemon.");
-pok();
-}  
+// }
+// function pikachu(pok) {
+//     console.log("Pikachu is an Electric type Pokemon.");
+// pok();
+// }  
 
-function charmander() {
-    console.log("Charmander is a Fire type Pokemon.");
-}
+// function charmander() {
+//     console.log("Charmander is a Fire type Pokemon.");
+// }
  
-console.log(pokemon("Pikachu", pikachu,charmander)); // Output: Pokemon Name: Pikachu
+// console.log(pokemon("Pikachu", pikachu,charmander)); // Output: Pokemon Name: Pikachu
+function avenger(name,power,power1){
+    console.log("Avenmgername:"+name);
+    power(power1);
+}
+function ironman(iron){
+    console.log("Rich,gineus,philantoriphist")
+    iron();
+} 
+function avenger1(spider,power1){
+console.log("Junior avenger name"+spider)
+power1();
+}
+function spiderman(e){
+    console.log("can climb on walls&building and can shoot webfluid");
+    e();
+
+}
+
+function eternalname(name,et){
+console.log("Eternal name"+name);
+et();
+}
+function eternal(more){
+console.log("they are the entity created by clestials to save humanity")
+more();
+}
+
+function clestials() {
+    console.log("Clestials are the most powerful beings in the universe");
+}
+avenger("Ironman", ironman, (next) =>avenger1("Spiderman", () =>spiderman(() =>eternalname("Thena",()=> eternal(() =>clestials())))))
+   
+
+;
