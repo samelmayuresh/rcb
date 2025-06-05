84d839469113a8076f7e9bda8c1a892eb1e547bc
samelmayuresh <mayureshsamel5@gmail.com>
2025-06-05 11:27:29 +0530

    new

--- sc.js
+++ sc.js
@@ -12,19 +12,18 @@
 // }
 // console.log(square(5)); // Output: 25
 
-function pokemon(name, type) {
+function pokemon(name, type1,type2) {
     console.log("Pokemon Name: " + name);
-    type();
+    type1(type2);
 
 }
-function pikachu() {
+function pikachu(pok) {
     console.log("Pikachu is an Electric type Pokemon.");
-
+pok();
 }  
 
 function charmander() {
     console.log("Charmander is a Fire type Pokemon.");
 }
  
-console.log(pokemon("Pikachu", pikachu)); // Output: Pokemon Name: Pikachu
-console.log(pokemon("Charmander", charmander)); // Output: Pokemon Name: Pikachu
+console.log(pokemon("Pikachu", pikachu,charmander)); // Output: Pokemon Name: Pikachu
