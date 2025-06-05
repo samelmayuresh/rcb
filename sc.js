15977b55946b3d7d50320205436f17d254e66d75
samelmayuresh <mayureshsamel5@gmail.com>
2025-06-05 10:12:32 +0530

    changes

--- sc.js
+++ sc.js
@@ -1,6 +1,9 @@
-let num=5;
-while(num<10){
-console.log(num);
-num+=2;
-}
-let num2=10;
+// let num=5;
+// while(num<10){
+// console.log(num);
+// num+=2;
+// }
+// let num2=10;
+const prompt = require('prompt-sync')();
+const name = prompt("What is your name? ");
+console.log("Hello, " + name + "!");
